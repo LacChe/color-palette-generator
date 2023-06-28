6dc6a2799fe7f87560367daabd16b12629408af9
LacChe <lacche96@gmail.com>
2023-06-28 13:55:48 +0800

    image scaling before processing

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { convertPixelArrayToRGB, quantizate } from './imageUtils/imageProcessor.js';
 
+const SHRINK_FACTOR = 0.1;
+
 let quantizedPixelArr = [];
 
 const imageInputButton = document.querySelector(".image-input-button");
@@ -25,13 +27,24 @@ function handleImageChange(e) {
         // process image on load
         const img = document.createElement("img");
         let imgOnLoadCallback = function imgOnLoadCallback() {
-            // create canvas element
+            // create canvas elements
             const cvs = document.createElement("canvas");
             const ctx = cvs.getContext("2d");
-            ctx.drawImage(img, 0, 0);
+
+            // scale image
+            let newWidth = 100 / SHRINK_FACTOR;
+            let newHeight = 100 / SHRINK_FACTOR;
+            if(img.width > img.height) {
+                newHeight *= img.height / img.width;
+            } else {
+                newWidth *= img.width / img.height;
+            }
+            ctx.canvas.width = newWidth;
+            ctx.canvas.height = newHeight;
+            ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
 
             // create rgb array
-            const pixelArr = ctx.getImageData(0, 0, img.width, img.height).data;
+            const pixelArr = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height).data;
             const rgbArr = convertPixelArrayToRGB(pixelArr);
             rgbArr.sort((p1, p2) => {
                 return p1.L - p2.L;
